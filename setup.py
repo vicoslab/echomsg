492721c2ba22bec178eaf3eed676755b450b7348
Luka Čehovin <luka.cehovin@gmail.com>
2017-04-18 22:20:04 +0200

    Raising version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='EchoMsg',
-	version='0.1.1',
+	version='0.1.2',
 	description='Message generator for echolib.',
 	author='Luka Cehovin',
 	author_email='luka.cehovin@gmail.com',
